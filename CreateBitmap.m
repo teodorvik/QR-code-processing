4f8eafb0b3e3a670f88ba1e2dfb0f6cd18b1f65d
Oscar Westberg <oscar.henrik.westberg@gmail.com>
2014-11-28 12:04:36 +0100

    Increased readability

--- CreateBitmap.m
+++ CreateBitmap.m
@@ -6,18 +6,14 @@ function[bitmap] = CreateBitmap(image)
 bitmap = zeros(41,41);
 [yMax, xMax] = size(image);
 
-% Used for debugging
-% figure;
-% imshow(image);
-% hold on;
-
+% The size of one QR-pixel in pixels
 stepSize = yMax / 41;
 
-% TODO: Possible to remove loop?
 for i = 1:41
     posX = round(stepSize * (i - 1)) + 1;
     posXNext = round(posX + stepSize);
     
+    % We don't want to go out of bounds
     if (posXNext > xMax)
         posXNext = xMax;
     end
@@ -33,14 +29,7 @@ for i = 1:41
         % Summarize a pixel area in the image
         tempImage = image(posY:posYNext, posX:posXNext); 
         bitmap(j,i) = mean(mean(tempImage));
-        
-        % Used for debugging
-%         plot(posX, posY, '-r+');
-%         plot(posXNext, posYNext, '-r+');
-        
     end
 end
 
 bitmap = bitmap > 0.5;
-
-%hold off;
